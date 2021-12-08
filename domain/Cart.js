3becb153183dbf5709259aa1a68bfa9d94689899
Mani Kumar Reddy Kancharla <mani.kancharla@kmanikumarreddy@live.com>
2021-12-08 20:51:50 +0530

    problem6 unique entity

--- domain/Cart.js
+++ domain/Cart.js
@@ -1,29 +1,31 @@
 const ActivityLog = require("./ActivityLog");
+const { v4: uuid } = require('uuid');
 
 class Cart {
-  constructor() {
-    this.items = [];
-    this.activityLog = new ActivityLog();
-  }
-
-  addItem(item) {
-    this.items.push(item);
-  }
-
-  removeItem(name) {
-    var index = 0;
-    var flag = false;
-    this.items.forEach((item, i) => {
-      if (item.product.name == name) {
-        index = i;
-        flag = true;
-      }
-    });
-    if (flag) {
-      this.items.splice(index, 1);
-      this.activityLog.recordItemRemoved(name);
+    constructor() {
+        this.items = [];
+        this.activityLog = new ActivityLog();
+        this.createdAt = uuid();
     }
-  }
+
+    addItem(item) {
+        this.items.push(item);
+    }
+
+    removeItem(name) {
+        var index = 0;
+        var flag = false;
+        this.items.forEach((item, i) => {
+            if (item.product.name == name) {
+                index = i;
+                flag = true;
+            }
+        });
+        if (flag) {
+            this.items.splice(index, 1);
+            this.activityLog.recordItemRemoved(name);
+        }
+    }
 }
 
 module.exports = Cart;
--- main.js
+++ main.js
@@ -3,27 +3,38 @@ const Product = require('./domain/Product');
 const Item = require('./domain/Item');
 
 main = () => {
-    const cart = new Cart();
-    console.log(`current State of Cart:`, cart);
+    const cart1 = new Cart();
+    console.log(`current State of Cart:`, cart1);
 
     const product1 = new Product('iPad Pro');
-    cart.addItem(new Item(product1, 1));
+    cart1.addItem(new Item(product1, 1));
 
-    console.log(`current State of Cart:`, cart);
+    console.log(`current State of Cart:`, cart1);
 
     const product2 = new Product('Hero ink Pen');
-    cart.addItem(new Item(product2, 1));
+    cart1.addItem(new Item(product2, 1));
 
-    console.log(`current State of Cart:`, cart);
+    console.log(`current State of Cart:`, cart1);
 
     const product3 = new Product('GM Cricket bat');
-    cart.addItem(new Item(product3, 2));
+    cart1.addItem(new Item(product3, 2));
 
-    console.log(`current State of Cart:`, cart);
+    console.log(`current State of Cart:`, cart1);
 
-    cart.removeItem('iPad Pro')
+    cart1.removeItem('iPad Pro')
 
-    console.log(`current State of Cart:`, cart);
+    console.dir([`current State of Cart:`, cart1], { depth: null });
+
+
+    // problem #6 add to multiple carts and ensure they are unique
+
+    const cart2 = new Cart();
+    cart2.addItem(new Item(product2, 1));
+
+    console.log(`\n\n#6\n cart1 === cart2 => ${cart1 === cart2}`);
+
+    console.dir([`current State of Cart 1:`, cart1], { depth: null });
+    console.dir([`current State of Cart 2:`, cart2], { depth: null });
 }
 
 main();
